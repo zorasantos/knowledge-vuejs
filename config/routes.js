d5656d99a5cf62db99cbad047b2a4026199ac09a
Zoranildo Santos <zoranildo22@gmail.com>
2020-02-16 18:36:09 -0300

    Conclusão api categorias

--- config/routes.js
+++ config/routes.js
@@ -9,6 +9,9 @@ module.exports = app => {
     app.route('/categories')
         .get(app.api.category.get)
         .post(app.api.category.save)
+
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
     
     app.route('/categories/:id')
         .get(app.api.category.getById)
